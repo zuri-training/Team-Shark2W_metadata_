3493eaee7c50046c1f14a02c5e22feb4da7b2bfd
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2022-12-12 19:38:32 +0100

    Added login and regsitration pages and also did some Javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+const showPassword = document.querySelector("#show-password");
+const hidePassword = document.querySelector("#hide-password");
+const passwordField = document.querySelector("#password");
+
+document.getElementById("hide-password").style.display = "none";
+
+showPassword.addEventListener("click", function () {
+  document.getElementById("show-password").style.display = "none";
+  document.getElementById("hide-password").style.display = "flex";
+  const type =
+    passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
+});
+
+hidePassword.addEventListener("click", function () {
+  document.getElementById("hide-password").style.display = "none";
+  document.getElementById("show-password").style.display = "flex";
+
+  const type =
+    passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
+});
